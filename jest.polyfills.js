bacd51cc360f1b703b8791ba0267e572d83893a8
r-sugi <r.sugimoto0615@gmail.com>
2024-03-30 00:34:35 +0900

    add: sample jest.polyfills.js

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -1,23 +1,15 @@
-// jest.polyfills.js
-/**
- * @note The block below contains polyfills for Node.js globals
- * required for Jest to function when running JSDOM tests.
- * These HAVE to be require's and HAVE to be in this exact
- * order, since "undici" depends on the "TextEncoder" global API.
- *
- * Consider migrating to a more modern test runner if
- * you don't want to deal with this.
- */
-
-const { TextDecoder, TextEncoder } = require('node:util')
+// see https://github.com/mswjs/msw/issues/1916#issuecomment-1845769931
+const { TextDecoder, TextEncoder } = require("node:util");
+const { ReadableStream } = require("web-streams-polyfill/ponyfill/es2018");
 
 Object.defineProperties(globalThis, {
+  ReadableStream: { value: ReadableStream },
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
-})
+});
 
-const { Blob, File } = require('node:buffer')
-const { fetch, Headers, FormData, Request, Response } = require('undici')
+const { Blob, File } = require("node:buffer");
+const { fetch, Headers, FormData, Request, Response } = require("undici");
 
 Object.defineProperties(globalThis, {
   fetch: { value: fetch, writable: true },
@@ -27,4 +19,4 @@ Object.defineProperties(globalThis, {
   FormData: { value: FormData },
   Request: { value: Request },
   Response: { value: Response },
-})
+});
